c67b25a308ef9b85ce5c06699256497b0b95461a
vincent.gendron <vincent.gendron@sully-group.fr>
2019-09-30 16:06:09 +0200

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo '# Vincent Gendron configuration'
+echo '# Vincent Gendron configuration' >> ~/.bashrc
 echo 'if [ -f /home/vincent.gendron/dotfiles/config/bashrc]; then' >> ~/.bashrc
 echo '    . /home/vincent.gendron/dotfiles/config/bashrc' >> ~/.bashrc
 echo 'fi' >> ~/.bashrc
