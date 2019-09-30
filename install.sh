fb35a4edf1b7632a1ea465695622b82512efc726
root@vm19044 <root@vm19044>
2019-09-30 15:55:08 +0200

    feat: Add installer.sh, bashrc config, tmux config, and micro binary

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo '# Vincent Gendron configuration'
+echo 'if [ -f /home/vincent.gendron/dotfiles/config/bashrc]; then' >> ~/.bashrc
+echo '    . /home/vincent.gendron/dotfiles/config/bashrc' >> ~/.bashrc
+echo 'fi' >> ~/.bashrc
