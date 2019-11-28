64a6a814696b50f2afd5e877d878c3fad7feb6bc
Vincent Gendron <vincent.gendron@sully-group.fr>
2019-11-28 17:41:55 +0100

    Updated with gitconfig

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Get current of the script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo '> Bashrc configuration'
 echo '# Vincent Gendron configuration' >> ~/.bashrc
 echo 'if [ -f /home/vincent.gendron/dotfiles/config/bashrc ]; then' >> ~/.bashrc
 echo '    . /home/vincent.gendron/dotfiles/config/bashrc' >> ~/.bashrc
 echo 'fi' >> ~/.bashrc
+
+echo '> Git configuration'
+rm -fi "~/.gitconfig"
+cp -f "$DIR/config/git/config" ~/.gitconfig
