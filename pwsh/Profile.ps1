c37523190a622ebb47900c8d1a2bb11d8f838156
Vincent Gendron <vincent.gendron@sully-group.fr>
2023-09-25 03:14:33 +0200

    feat: DE configured

--- pwsh/Profile.ps1
+++ pwsh/Profile.ps1
@@ -3,3 +3,10 @@ Set-PSReadlineOption -ShowToolTips
 
 Invoke-Expression (&starship init powershell)
 
+function New-Workspace {komorebic.exe new-workspace}
+
+Set-Alias -Name WM -Value komorebic.exe
+
+function Home {cd ~}
+
+function Start-WM { komorebic start -c ~/komorebi.json --whkd }
